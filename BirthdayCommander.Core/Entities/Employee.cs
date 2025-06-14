907d3cb6ffc25dd5f7ac518bea83555483b7e577
malinborn <rikkeisy@gmail.com>
2025-06-14 16:37:58 +0300

    made some props nullable

--- BirthdayCommander.Core/Entities/Employee.cs
+++ BirthdayCommander.Core/Entities/Employee.cs
@@ -5,11 +5,11 @@ namespace BirthdayCommander.Core.Entities;
 public class Employee
 {
     public Guid Id { get; set; }
-    public string email { get; set; }
-    public DateTime Birthday { get; set; }
-    public string MattermostUserId { get; set; }
+    public string Email { get; set; }
+    public DateTime? Birthday { get; set; }
+    public string? MattermostUserId { get; set; }
     public bool IsActive { get; set; }
-    public string WishlistLink { get; set; }
+    public string? WishlistLink { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
     
